5bfa9d5e768f5b811a9536463edb5ad977f64ce4
Gartland-R2 <Gartland-R2>
2022-12-07 10:56:33 +0000

    updated url path

--- src/services/video.service.js
+++ src/services/video.service.js
@@ -5,7 +5,7 @@ import UploadVideo from "../components/upload-video/upload-video.component";
 class VideoService {
   getAllVideos() {
     const API_URL =
-      "https://a2-backend.azurewebsites.net/api/v1.0/videos?code=XDnK65rmY2AA3F1DyjVT5N3jy6-3o3uBIvJFOx8BZ_I6AzFuh5XMLA==";
+      "https://a2-backend.azurewebsites.net/api/v1.0/videos?code=p00QulcvNEgpjiG2UO-gshPMYPnS22QNnYng7rPoFBNtAzFu6MtCbg==";
     return axios.get(API_URL).then((resp) => {
       console.log(resp.data);
       return resp.data;
@@ -15,7 +15,7 @@ class VideoService {
   getVideoByID(videoID) {
     const API_URL1 = "https://a2-backend.azurewebsites.net/api/v1.0/videos/";
     const API_URL2 =
-      "?code=Lq8mcgiQLkSLKT-Q8SZWKMbsbPfOgC61chU0GUHtCerHAzFuifHylg==";
+      "?code=FovacDFVriiz6RJzohGD8EfEINeDhL8bBKr37Dlt015QAzFuHMvLSQ==";
     console.log(API_URL1 + videoID + API_URL2);
     return axios.get(API_URL1 + videoID + API_URL2).then((resp) => {
       return resp.data;
